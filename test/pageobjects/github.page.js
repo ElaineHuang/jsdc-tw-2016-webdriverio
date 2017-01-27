c3b9fc7832ddec240d42929335dcb62d7f8761ad
ElaineHuang <starbrave1313677@gmail.com>
2017-01-27 16:41:00 +0800

    add focus action

--- test/pageobjects/github.page.js
+++ test/pageobjects/github.page.js
@@ -28,7 +28,7 @@ class GithubPage extends Page {
     get userDropdown() { return browser.element('#user-links > li:nth-child(3) > a.header-nav-link'); }
     get yourStars() { return browser.element('#user-links > li.header-nav-item.dropdown.js-menu-container.active > div > div > a:nth-child(4)'); }
     get unStarBtn() { return browser.element('#js-pjax-container > div > div.col-9.float-left.pl-2 > div.js-repo-filter.position-relative > div:nth-child(2) > div.float-right > div > form.starred > button'); }
-
+    get deleteForm() { return browser.element('.js-normalize-submit'); }
     open() {
         super.open('https://github.com/');
         this.headerLoaded.waitForExist();
@@ -58,6 +58,8 @@ class GithubPage extends Page {
         this.settingDelete.click();
         this.settingModal.waitForExist();
         this.settingModalRename.setValue(repositoryName);
+        this.settingModalRename.leftClick(10, 10);
+        this.settingModalDelete.waitForEnabled(1000);
         this.settingModalDelete.click();
         this.indexFlashNotice.waitForExist();
     }
